bf9b3a32dfce9b6354e36b855146c7f540ec53e8
Truls Asheim <truls@asheim.dk>
2015-12-22 18:29:05 +0100

    Add support for "sellable" and "refundable" (e.g. bottle refund) stock

--- inventory/admin.py
+++ inventory/admin.py
@@ -6,7 +6,7 @@ class VendorAdmin(admin.ModelAdmin):
     fields = ['name']
 
 class ProductAdmin(admin.ModelAdmin):
-    fields = ['name', 'vendor', 'quantity', 'discontinued']
+    fields = ['name', 'vendor', 'quantity', 'category', 'discontinued']
 
 admin.site.register(Vendor, VendorAdmin)
 admin.site.register(User)
